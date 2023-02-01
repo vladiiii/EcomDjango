1b67cea4258e6e289d9ea5fe30fbe9575d496092
Vladi <vladiiiiiiiii@gmail.com>
2023-02-01 17:58:20 +0300

    added store app

---
+++ store/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    user = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
