03a40e681c57fd8da1439b799dac10db58d8e72f
Vladi <vladiiiiiiiii@gmail.com>
2023-02-01 22:28:00 +0300

    added product detail

--- petnet/urls.py
+++ petnet/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core.views import front_page, about
 
 urlpatterns = [
+    path("", include("store.urls")),
     path("", front_page, name="frontpage"),
     path("about/", about, name="about"),
     path("admin/", admin.site.urls),
