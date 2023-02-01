2b3d88ef2066a1cc423b72c44166232c92767e4d
Vladi <vladiiiiiiiii@gmail.com>
2023-02-01 15:49:14 +0300

    added basic templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def front_page(request):
+    return render(request, "core/frontpage.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
