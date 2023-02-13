d5bf1a6267c1d7f33d28773bb4ad889d4233b58d
Vladi <vladiiiiiiiii@gmail.com>
2023-02-13 17:24:22 +0300

    added vendor settings

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
+from django.utils.text import slugify
 from django.contrib.auth.models import User
 
 from .models import UserProfile
+from store.forms import ProductForm
+from store.models import Product
 
 
 def vendor_detail(request, pk):
@@ -12,6 +16,47 @@ def vendor_detail(request, pk):
     return render(request, "userprofile/vendor_detail.html", {"user": user})
 
 
+@login_required
+def my_store(request):
+    return render(request, "userprofile/my_store.html")
+
+
+@login_required
+def add_product(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            title = request.POST.get("title")
+
+            product = form.save(commit=False)
+            product.user = request.user
+            product.slug = slugify(title)
+            product.save()
+
+            return redirect("my_store")
+    else:
+        form = ProductForm()
+
+    return render(request, "userprofile/add_product.html", {"title": "Add Product", "form": form})
+
+
+@login_required
+def edit_product(request, pk):
+    product = Product.objects.filter(user=request.user).get(pk=pk)
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=product)
+
+        if form.is_valid():
+            form.save()
+            return redirect("my_store")
+    else:
+        form = ProductForm(instance=product)
+
+    return render(request, "userprofile/add_product.html", {"title": "Edit Product", "form": form})
+
+
+@login_required
 def myaccount(request):
     return render(request, "userprofile/myaccount.html")
 
