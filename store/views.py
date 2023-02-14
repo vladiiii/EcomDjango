4d934bd9f5110345abd41d3ff8a3ffcf12c1410b
Vladi <vladiiiiiiiii@gmail.com>
2023-02-14 13:22:25 +0300

    added basic cart

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .cart import Cart
 from .models import Product, Category
 
 
@@ -21,3 +23,10 @@ def category_detail(request, slug):
 def product_detail(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug, status=Product.ACTIVE)
     return render(request, "store/product_detail.html", {"product": product})
+
+
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    cart.add(product_id)
+
+    return redirect("frontpage")
